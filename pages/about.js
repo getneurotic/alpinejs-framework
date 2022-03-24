45f221291f5ba972047c3277434936f72839eb69
Jóhannes Rónaldsson <g.stekelenburg@outlook.com>
2022-03-24 01:32:53 +0000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,13 @@ export default Alpine.component('AboutPage', () => ({
 	template() {
 		return (
 			<Layout>
-				<h1 class="text-xl capitalize">This is a title</h1>
+				<div class="flex flex-col text-center space-y-4">
+					<h1 class="text-xl capitalize">About AlpineJS Framework</h1>
+					<p>
+						It's a way of  living.
+					</p>
+					<a href="/#" class="underline hover:text-blue-500">Back to Home</a>
+				</div>
 			</Layout>
 		)
 	}
